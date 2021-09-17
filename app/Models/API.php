ac3447962cb8e3ba21b21b3e539ee95760cecdfd
Gowtham <mahesinfotech2012@gmail.com>
2021-09-17 11:26:58 +0530

    my changes

---
+++ app/Models/API.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class API extends Model
+{
+    use HasFactory;
+    protected $table='a_p_i_s';
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+        /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
---
+++ database/migrations/2021_09_17_044044_create_a_p_i_s_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAPISTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('a_p_i_s', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('email');
+            $table->text('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('a_p_i_s');
+    }
+}
